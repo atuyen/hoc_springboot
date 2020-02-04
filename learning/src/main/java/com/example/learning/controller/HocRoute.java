6bfac9edfabf42a782ac7d0717f5995f05efa171
tuyen nguyen <tuyen.nguyen@sea-solutions.com>
2020-02-04 22:37:20 +0700

    3_2: Them tien to chu url

--- learning/src/main/java/com/example/learning/controller/HocRoute.java
+++ learning/src/main/java/com/example/learning/controller/HocRoute.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HocRoute {
-    //Muc co ban nhat ===> api localhost:8080
+    //Muc co ban nhat ===> api localhost:8080/api
     @RequestMapping("/")
     String home() {
         return "Muc co ban nhat";
     }
 
 
-    //Muc co ban 2 ===> api localhost:8080/simple
+    //Muc co ban 2 ===> api localhost:8080/api/simpleapi
     @RequestMapping("/simple")
     String simple() {
         return "Muc co ban 2";
--- learning/src/main/java/com/example/learning/controller/HocRoute2.java
+++ learning/src/main/java/com/example/learning/controller/HocRoute2.java
@@ -1,21 +1,26 @@
 package com.example.learning.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(path = "/v1")
 @RestController
 public class HocRoute2 {
+    @Value("${test}")
+    private  String test;
     //Muc co ban nhat ===> api localhost:8080/v1/
     @RequestMapping("/")
     String home() {
+
+
         return "Muc co ban nhat cua v1";
     }
 
     //Muc co ban nhat ===> api localhost:8080/v1/simple
     @RequestMapping("/simple")
     String simple() {
-        return "Muc simple cua v1";
+        return "Muc simple cua v1 "+test;
     }
 }
